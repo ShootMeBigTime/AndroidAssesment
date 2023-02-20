d59471f89708f323039fbbd6c17307435fea572b
ShootMeBigTime <70698888+ShootMeBigTime@users.noreply.github.com>
2023-02-20 16:48:39 +0100

    Zooi

--- app/src/main/java/com/example/assesment/MainActivity.java
+++ app/src/main/java/com/example/assesment/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.assesment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -13,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void onItemSelected(String pokemon){
 
 
     }
